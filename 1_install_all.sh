116a88e55aeb1abd37797416f7919e626caa29e5
Zarif <mohamadzarif96@gmail.com>
2024-02-16 08:32:05 +0800

    Update new .bashrc

--- 1_install_all.sh
+++ 1_install_all.sh
@@ -7,7 +7,7 @@ cp ~/.bashrc ~/.bashrc.original
 meld .bashrc ~/.bashrc &
 
 cp .vimrc ~/.
-gvim ~/.vimrc -c ":PlugInstall | :qa"
+gvim ~/.vimrc -c ":PlugInstall"
 
 chmod 775 3_install_nvim.sh
 ./3_install_nvim.sh 
