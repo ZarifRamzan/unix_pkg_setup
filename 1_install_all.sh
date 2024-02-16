a88ae70a4c7ab2399b28172ae85fdffdc34304d6
Zarif <mohamadzarif96@gmail.com>
2024-02-16 08:19:18 +0800

    Update command

--- 1_install_all.sh
+++ 1_install_all.sh
@@ -3,13 +3,12 @@
 chmod 775 2_install_unix_packages.sh
 ./2_install_unix_packages.sh  
 
-chmod 775 3_install_nvim.sh
-./3_install_nvim.sh 
-
-cd ~
 cp ~/.bashrc ~/.bashrc.original
 meld .bashrc ~/.bashrc &
 
 cp .vimrc ~/.
-nvim ~/.vimrc -c ":PlugInstall | :qa"
+gvim ~/.vimrc -c ":PlugInstall | :qa"
+
+chmod 775 3_install_nvim.sh
+./3_install_nvim.sh 
 
