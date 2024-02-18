3ccc84d740336ec9a9ea7b8d30eb9879fe6bc6eb
Zarif <mohamadzarif96@gmail.com>
2024-02-18 10:41:50 +0800

    Update 4_vimrc_setup.sh

--- 4_vimrc_setup.sh
+++ 4_vimrc_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 rm ~/.vimrc
+cp ~/unix_pkg_setup/vimrc ~/.
 mv ~/unix_pkg_setup/vimrc ~/unix_pkg_setup/.vimrc 
-cp ~/unix_pkg_setup/.vimrc ~/.
 chmod 775 ~/.vimrc
 gvim ~/.vimrc -c ":PlugInstall"
