900d8ac7a4677289a5a8db173aeded47cb98e9b6
Zarif <mohamadzarif96@gmail.com>
2024-03-09 16:35:36 +0800

    Update 1_install_all.sh

--- 1_install_all.sh
+++ 1_install_all.sh
@@ -2,17 +2,17 @@
 
 export DIR=$(pwd)
 
-chmod 775 2_install_unix_packages.sh
+chmod 775 $DIR/2_install_unix_packages.sh
 ./2_install_unix_packages.sh  
 
-chmod 775 3_bashrc_setup.sh
+chmod 775 $DIR/3_bashrc_setup.sh
 ./3_bashrc_setup.sh 
 
-chmod 775 4_vimrc_setup.sh
+chmod 775 $DIR/4_vimrc_setup.sh
 ./4_vimrc_setup.sh  
 
-chmod 775 5_install_nvim.sh
+chmod 775 $DIR/5_install_nvim.sh
 ./5_install_nvim.sh
 
-chmod 775 7_inputrc_setup
+chmod 775 $DIR/7_inputrc_setup
 ./7_inputrc_setup
