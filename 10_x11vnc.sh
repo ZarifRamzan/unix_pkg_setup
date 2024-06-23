af400c82e45883895bae2e6f1ce258f85f299ada
Zarif <mohamadzarif96@gmail.com>
2024-06-23 09:00:40 +0800

    Update 10_x11vnc.sh

--- 10_x11vnc.sh
+++ 10_x11vnc.sh
@@ -1,5 +1,24 @@
-## Ref website: 
-
 #!/bin/bash
-sudo apt-get install x11vnc
-echo "1234" x11vnc -storepasswd -f 
+
+# Set the directory to the current working directory
+export DIR=$(pwd)
+
+# Install x11vnc if it's not already installed
+sudo apt-get install -y x11vnc
+
+# Create the .vnc directory if it does not exist
+mkdir -p "$DIR/.vnc"
+
+# Store the password in each existing file in the .vnc directory
+for file in "$DIR/.vnc/"*; 
+do
+    # Ensure we are dealing with a file
+    if [ -f "$file" ]; then
+        #!!!Change password here!!!
+        echo "1234" | x11vnc -storepasswd -f "$file"
+        #chmod 600 "$file"
+        echo "Password has been stored in $file"
+    fi
+done
+
+echo "x11vnc password has been stored in all files within $DIR/.vnc/"
